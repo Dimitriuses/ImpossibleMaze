439a77b62eb831809c03e9d03eed74123248391d
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2021-09-23 13:51:22 +0300

    add pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenu;
+    public void TogglePause(bool togle)
+    {
+        PauseMenu.SetActive(togle);
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause(!PauseMenu.active);
+        }
+    }
+}
